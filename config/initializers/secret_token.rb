f57dc5a295c93afdc7ddc8e45c2d9c08d52b896b
Dustin Ngo <dustin@nationbuilder.com>
2014-07-28 10:57:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd34c47286f34e561c2dc8cf0d50a2360582cf2c82b96bd12baf60c293cd8487276db6b247cb715376f46e12f73af4dd5bf315bc3c21e16e6a2c4f1db0f73eddf'
